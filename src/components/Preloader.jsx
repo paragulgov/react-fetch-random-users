8282ea5abe2b45c776f29fea6cdc3044ada0e62f
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2021-01-06 16:04:00 +0300

    preloader

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Preloader = () => {
+   return (
+      <section>
+         <span className="load"></span>
+      </section>
+   )
+}
+
+export default Preloader
